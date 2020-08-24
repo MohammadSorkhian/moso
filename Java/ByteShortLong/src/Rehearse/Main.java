657f85672923896264c4bf37718105b056b72fc0
MohammadSorkhian <mohammadsorkhian@gmail.com>
2020-08-24 19:42:27 -0230

    convert string to int

---
+++ Java/ByteShortLong/src/Rehearse/Main.java
@@ -0,0 +1,8 @@
+package Rehearse;
+public class Main {
+    public static void main(String[] args) {
+        String num = "2020";
+        int num2 = Integer.parseInt(num);
+        System.out.println(num2);
+    }
+}
---
+++ Java/ByteShortLong/src/Rehearse/moso.java
@@ -0,0 +1,11 @@
+package Rehearse;
+
+public class moso {
+    public static void switchValue(){
+        int value = 1;
+        switch (value){
+            case 1:
+                System.out.println("it is ");
+        }
+    }
+}
