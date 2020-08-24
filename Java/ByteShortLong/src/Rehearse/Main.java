35ed0b46175a0b7adcbf960ffdfb84cbd0e1a930
MohammadSorkhian <msorkhiankho@mun.ca>
2020-08-24 21:10:38 -0230

    continous input

--- Java/ByteShortLong/src/Rehearse/Main.java
+++ Java/ByteShortLong/src/Rehearse/Main.java
@@ -8,32 +8,48 @@ public class Main {
 //        int num2 = Integer.parseInt(num);
 //        System.out.println(num2);
 
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Please enter your year of birth : ");
+//        boolean hasNextInt = scanner.hasNextInt();
+//        while (!hasNextInt){
+//            System.out.println("Sorry the entered value is incorrect " +
+//                    "Please enter your year of birth : ");
+//            scanner.nextLine();
+//            hasNextInt = scanner.hasNextInt();
+//        }
+//        int yearOfBirth = scanner.nextInt();
+//
+//
+//
+//        scanner.nextLine();
+//
+//        System.out.println("Please enter your name : ");
+//        String name = scanner.nextLine();
+//
+//        int age = 2020 - yearOfBirth;
+//
+//        scanner.close();
+//
+//        System.out.println(name + "'s age is " + age);
+
+        System.out.println("Enter your nums");
         Scanner scanner = new Scanner(System.in);
+        double sum = 0;
 
-        System.out.println("Please enter your year of birth : ");
-        boolean hasNextInt = scanner.hasNextInt();
-        while (!hasNextInt){
-            System.out.println("Sorry the entered value is incorrect " +
-                    "Please enter your year of birth : ");
-            scanner.nextLine();
-            hasNextInt = scanner.hasNextInt();
+        while(true){
+            boolean isNum = scanner.hasNextInt();
+            if (isNum){
+                sum += scanner.nextInt();
+                scanner.nextLine();
+            }else{
+                System.out.println(sum);
+                break;
+            }
+
         }
-        int yearOfBirth = scanner.nextInt();
-
-
-
-        scanner.nextLine();
-
-        System.out.println("Please enter your name : ");
-        String name = scanner.nextLine();
-
-        int age = 2020 - yearOfBirth;
 
         scanner.close();
-
-        System.out.println(name + "'s age is " + age);
-
-
 
 
 
