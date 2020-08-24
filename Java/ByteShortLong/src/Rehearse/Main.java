5fbe8fb5ada4b3959a0da5a97d7977cefd21ac4e
MohammadSorkhian <msorkhiankho@mun.ca>
2020-08-24 19:43:33 -0230

    String to int

--- Java/ByteShortLong/src/Rehearse/Main.java
+++ Java/ByteShortLong/src/Rehearse/Main.java
@@ -1,8 +1,10 @@
 package Rehearse;
 public class Main {
     public static void main(String[] args) {
-        String num = "2020";
-        int num2 = Integer.parseInt(num);
-        System.out.println(num2);
+//        String num = "2020";
+//        int num2 = Integer.parseInt(num);
+//        System.out.println(num2);
+
+        
     }
 }
