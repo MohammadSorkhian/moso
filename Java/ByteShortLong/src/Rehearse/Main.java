6117795d34fecfffcfa6cce88844c1e9cf4b7406
MohammadSorkhian <msorkhiankho@mun.ca>
2020-08-25 22:18:44 -0230

    real-imaginary num

--- Java/ByteShortLong/src/Rehearse/Main.java
+++ Java/ByteShortLong/src/Rehearse/Main.java
@@ -4,11 +4,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int x = 10;
+        
 
-        moso.VipPerson person1 = new moso.VipPerson();
-        moso.VipPerson person2 = new moso.VipPerson("bob",2000, "moso@y.com");
-        System.out.println(person1.getCreditLimit());
-        System.out.println(person2.getCreditLimit());
 
       }
 }
--- Java/ByteShortLong/src/Rehearse/moso.java
+++ Java/ByteShortLong/src/Rehearse/moso.java
@@ -1,44 +1,10 @@
 package Rehearse;
 
+import org.w3c.dom.ls.LSOutput;
+
+import javax.crypto.spec.PSource;
+
 public class moso {
 
-    public static class VipPerson {
-        private String name;
-        private double creditLimit;
-        private String emailAddress;
 
-        public VipPerson(){
-            this("Default Name", 0000, "Default EmailAddress");
-        }
-
-        public VipPerson(String name, double creditLimit, String emailAddress) {
-            this.name = name;
-            this.creditLimit = creditLimit;
-            this.emailAddress = emailAddress;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public double getCreditLimit() {
-            return creditLimit;
-        }
-
-        public void setCreditLimit(double creditLimit) {
-            this.creditLimit = creditLimit;
-        }
-
-        public String getEmailAddress() {
-            return emailAddress;
-        }
-
-        public void setEmailAddress(String emailAddress) {
-            this.emailAddress = emailAddress;
-        }
-        }
     }
