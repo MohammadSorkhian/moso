ccbae68f87ff547771709468274ee0de1f38a93d
Mohammad <mohammadsorkhian@gmail.com>
2020-11-21 02:49:59 -0330

    different

---
+++ C#/TrackerLibrary/TrackerLibrary/TournamentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    class TournamentModel
+    {
+        public string Tournament { get; set; }
+        public decimal EntryFee { get; set; }
+        public List<TeamModel> enteredTeams { get; set; } = new List<TeamModel>();
+        public List<PrizeModel> Prizes { get; set; } = new List<PrizeModel>();
+        public List<List<MatchupMoleds>> Rounds { get; set; } = new List<List<MatchupMoleds>>();
+
+    }
+}
