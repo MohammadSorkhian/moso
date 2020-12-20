319efbb05e9deb540994173620d93473900ff934
Mohammad <mohammadsorkhian@gmail.com>
2020-12-19 23:56:54 -0330

    add Images section and their animation

---
+++ C#/Reherce_Project/Temp/ImagePost.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Temp
+{
+    class ImagePost : Post
+    {
+        public string ImageURL { get; set; }
+        public ImagePost()
+        {
+            this.ImageURL = "c:/";
+        }
+
+        public ImagePost(string title, string sentByName, bool isPublic, string url)
+        {
+            this.Title = title;
+            this.SendByUserName = sentByName;
+            this.Ispublic = isPublic;
+            this.ImageURL = url;
+        }
+
+        public override string ToString()
+        {
+            return ($"{this.ID} - {this.Title} by {this.SendByUserName} in address: {ImageURL}");
+        }
+    }
+}
--- C#/Reherce_Project/Temp/post.cs
+++ C#/Reherce_Project/Temp/post.cs
@@ -8,19 +8,21 @@ namespace Temp
     class Post
     {
         private static int CurrrentPostId;
-        public int ID { get; set; }
-        public string Title { get; set; }
-        public string SendByUserName { get; set; }
-        public bool Ispublic { get; set; }
+        protected int ID { get; set; }
+        protected string Title { get; set; }
+        protected string SendByUserName { get; set; }
+        protected bool Ispublic { get; set; }
 
+        //Constructor Empty
         public Post()
         {
-            ID = 0;
+            ID = GetNextID();
             Title = "My first Post";
             Ispublic = true;
             SendByUserName = "Moso";
         }
 
+        //Constructor 3 Parameters
         public Post(string title, string sendByUserName, bool isPublic)
         {
             this.ID = GetNextID();
@@ -29,9 +31,22 @@ namespace Temp
             this.Ispublic = Ispublic;
         }
 
+        //Generates ID
         protected int GetNextID()
         {
             return ++CurrrentPostId;
         }
+
+        public void Update(string title, bool isPublic)
+        {
+            this.Title = title;
+            this.Ispublic = isPublic;
+        }
+
+        public override string ToString()
+        {
+            return ($"{this.ID} - {this.Title} by {this.SendByUserName}");
+        }
+
     }
 }
