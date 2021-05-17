77728318d33061c9b5dc926ac81a60aa638587af
MohammadSorkhian <52132076+MohammadSorkhian@users.noreply.github.com>
2021-05-17 19:44:30 -0230

    Delete Nodejs directory

--- Nodejs/notes-app/app.js
+++
@@ -1,8 +0,0 @@
-const chalk = require("chalk")
-const y = require("yargs")
-const validator = require("validator")
-const yargs = require("yargs")
-
-console.log(process.argv)
-console.log(yargs.argv)
-yargs.version("1.1")
